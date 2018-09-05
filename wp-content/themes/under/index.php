89fe7045d1070426439b5237356bf10379b53314
qrames <ramque@orange.fr>
2018-09-05 16:44:53 +0200

    the hover color

--- wp-content/themes/under/index.php
+++ wp-content/themes/under/index.php
@@ -21,18 +21,9 @@ get_header();
 		<?php
 		if ( have_posts() ) :
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
-
-			/* Start the Loop */
 			while ( have_posts() ) :
 				the_post();
-
+                echo the_post();
 				/*
 				 * Include the Post-Type-specific template for the content.
 				 * If you want to override this in a child theme, then include a file
